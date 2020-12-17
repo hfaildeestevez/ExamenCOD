543c1fa938181aec40dc0d75f2f28ef75ce244db
hfaildeestevez <hfaildeestevez@danielcastelao.org>
2020-12-17 19:02:09 +0100

    Commit JAVADOC

--- src/com/cod/Comprobacion.java
+++ src/com/cod/Comprobacion.java
@@ -2,20 +2,22 @@ package com.cod;
 
 public class Comprobacion {
     private static Comprobacion instance=null;
+    /** Declaramos variables de la clase **/
     String user = "";
     String ip = "127.0.0.2";
-
+    /** Declaramos constructor **/
    public Comprobacion() {
         user = "anonymous@danielcastelao.org";
     }
-public static Comprobacion getInstance(){
+    /** Metodo **/
+    public static Comprobacion getInstance(){
        if (instance==null) {instance= new Comprobacion(); }
        return instance;}
 
    Comprobacion(String parametro1) {
         this.user = parametro1;
     }
-
+    /** Segundo Metodo **/
     public boolean con() {
         if (user != "anonymous@danielcastelao.org") {
             return true;
--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+        /** explicamos condiciones que nos dan fallo y acierto **/
         if (verConexion1("pepe@danielcastelao.org")) {
             System.out.println("Listo");
         } else {
@@ -17,13 +18,13 @@ public class Main {
         }
         ;
     }
-
+    /** Primer metodo para ver el primer usuario conectado con instancia para singleton**/
     public static boolean verConexion1(String user) {
         Comprobacion obx1 =  Comprobacion.getInstance();
         System.out.println("Conectando a " + obx1.ip + ", con el usuario " + user);
         return obx1.con();
     }
-
+    /** Segundo metoo para ver el segundo usuario conectado con instancia para singleton**/
     public static boolean verConexion2() {
         Comprobacion obx2 =  Comprobacion.getInstance();
         System.out.println("Conectando a " + obx2.ip + ", con el usuario " + obx2.user);
